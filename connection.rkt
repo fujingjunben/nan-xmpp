1d1c1127b4034edf0e68211926ed7aeeff0a3ba4
Marisa Kirisame <marisa.magician@gmail.com>
2012-12-31 17:23:12 +0900

    connection added

---
+++ connection.rkt
@@ -0,0 +1,55 @@
+#lang racket/base
+
+(require racket/contract
+	 racket/tcp ;; networking
+	 openssl    ;; ssl/tls
+	 "utils.rkt")
+
+(define-struct connection (host i-port o-port custodian)
+  #:mutable)
+
+(provide/contract
+ [struct connection
+         ([host string?]
+          [i-port input-port?]
+          [o-port output-port?]
+          [custodian custodian?])]
+ [send-string (connection? string? . -> . any)]
+ [new-connection (string? . -> . connection?)]
+ [kill-connection! (connection? . -> . void)])
+
+(define port 5222)
+(define ssl-port 5223)
+
+;;;; ;; ;  ;;;  ;
+;;
+;; tls & sasl
+;;  - http://xmpp.org/rfcs/rfc3920.html#tls
+;;  - http://xmpp.org/rfcs/rfc3920.html#sasl
+;;
+;;;; ;;
+
+
+;; moved to xmpp-sasl until it 'works'
+
+(define session->tls? #f) ;; changes state when a tls proceed is recived
+
+(define (send-string out str)
+  (debugf "sending: ~a ~%~%" str) 
+    (fprintf out "~A~%" str) (flush-output out))
+
+(define (new-connection host)
+    (define conn-cust (make-custodian))
+    (parameterize ([current-custodian conn-cust])
+      (let-values ([(in out)
+		    (ssl-connect host ssl-port 'tls)])
+	(file-stream-buffer-mode out 'line)
+	(make-connection
+	 host in out conn-cust))))
+
+(define (kill-connection! conn)
+  (with-handlers ([exn:fail:network? void])
+    (close-output-port (connection-o-port conn)))
+  (with-handlers ([exn:fail:network? void])
+    (close-input-port (connection-i-port conn)))
+  (custodian-shutdown-all (connection-custodian conn)))
