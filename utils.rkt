a384750b26458695e2ad1ec4c109a117422b3740
poi519 <iamyou519@mail.ru>
2013-01-04 01:28:43 +0200

    xmpp-receive

--- utils.rkt
+++ utils.rkt
@@ -1,11 +1,13 @@
 #lang racket/base
 (require racket/list
-	 racket/contract) 
+	 racket/contract
+         racket/string) 
 
 (provide (contract-out [hash-append (hash? hash? . -> . hash?)]
 		       [read-async (port? . -> . string?)]
 		       [read-async-bytes (port? . -> . (listof byte?))])
-         debug? debugf)
+         debug? debugf
+         clean)
 
 ;;;; ;  ;;  ;
 ;; 
@@ -42,3 +44,12 @@
       (let* ([first-key (first (hash-keys hs-2))]
 	     [first-value (hash-ref hs-2 first-key)])
 	(hash-append (hash-set hs-1 first-key first-value) (hash-remove hs-2 first-key)))))
+
+(define (remove-xml-header str)
+  (regexp-replace (regexp "<\\?.+\\?>") str ""))
+
+(define (remove-stream-header str)
+  (regexp-replace (regexp "<stream:stream[^<>]+>") str ""))
+
+(define (clean str)
+  (string-trim (remove-stream-header (remove-xml-header str))))
